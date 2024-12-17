6bee5366061eb1c43b0ba5a6d90e912848cc9884
Yoonsuh Kim <blawesomek@gmail.com>
2024-12-17 11:56:05 +0900

    review_provider.dart

--- lib/pages/favorite_counselors_page.dart
+++ lib/pages/favorite_counselors_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'favorite_counselor_provider.dart';
 import 'package:renewus/providers/favorite_counselor_provider.dart';
 
 class FavoriteCounselorsPage extends StatelessWidget {
